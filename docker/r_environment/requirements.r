6d0ed8aefaaeb922f182b1cd1c9bc9ab6e3e2a81
MJ-OneEqualsOne <mj@oneequalsone.uk>
2024-04-03 14:19:16 +0100

    adding requirements to r

---
+++ docker/r_environment/requirements.r
@@ -0,0 +1,15 @@
+# Install R packages
+install.packages(c(
+    "tidyverse",   # Collection of R packages for data science (ggplot2, dplyr, etc.)
+    "shiny",       # Web application framework for R
+    "shinythemes", # Themes for Shiny applications
+    "DT",          # R Interface to the jQuery Plug-in DataTables
+    "plotly",      # Create interactive web graphics from R via plotly's JavaScript graphing library
+    "rmarkdown",   # Dynamic documents for R
+    "devtools",    # Tools to make developing R packages easier
+    "testthat",    # Testing framework for R
+    "knitr",       # General-purpose literate programming engine
+    "lubridate",   # Date-time manipulation
+    "janitor"      # Simple tools for examining and cleaning dirty data
+    # Add any other packages your projects might require
+))
